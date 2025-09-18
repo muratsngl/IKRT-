92056143ff5096a6978c2e28b8f042bffd13c5a5
muratsngl <sengul.murat@metu.edu.tr>
2025-09-18 14:20:12 +0300

    tasiniyoruz

--- src/main_new.cpp
+++ src/main_new.cpp
@@ -32,12 +32,12 @@ int main() {
     //     }
     //     return -1;
     // }
-    // if(load_scene_element_model("assets/models/watermelon/scene.gltf") == false){
-    //     std::cerr << "Failed to load scene element model" << std::endl;
-    //     cleanup_rendering();
-    //     cleanup_shared_memory();
-    //     return -1;
-    // } //commeted out to flip the scene until the scenelementdata is being implemented
+    if(load_scene_element_model("assets/models/robot/robot_from_the_series_love_death_and_robots.glb") == false){
+        std::cerr << "Failed to load scene element model" << std::endl;
+        cleanup_rendering();
+        cleanup_shared_memory();
+        return -1;
+    } //commeted out to flip the scene until the scenelementdata is being implemented
     
     
     // if(load_interactable_model("assets/models/mug/mugR.glb")==false){
